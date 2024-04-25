96eb5a4a05e3678b63a038ce857074dc5a632b4f
Sergei Dudnik (on Mac) <bigbulk@gmail.com>
2024-04-25 19:03:12 +0300

    changes in index.php and Generator.php

--- public/index.php
+++ public/index.php
@@ -3,15 +3,55 @@
 // Подключение автозагрузки через composer
 require __DIR__ . '/../vendor/autoload.php';
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+
+function generate($count)
+{
+    $numbers = range(1, 100);
+    shuffle($numbers);
+
+    $faker = \Faker\Factory::create();
+    $faker->seed(1);
+    $companies = [];
+    for ($i = 0; $i < $count; $i++) {
+        $companies[] = [
+            'id' => $numbers[$i],
+            'name' => $faker->company,
+            'phone' => $faker->phoneNumber
+        ];
+    }
+
+    return $companies;
+}
+
+$companies = generate(100);
 
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
+    return $response->write('go to the /companies');
 });
+
+// BEGIN (write your solution here)
+$app->get('/companies', function ($request, $response) use ($companies) {
+    $page = $request->getQueryParam('page', 1);
+    $per = $request->getQueryParam('per', 5);
+    $pagedArray = array_chunk($companies, $per);
+    $nthPage = $pagedArray[$page - 1];
+    $response->getBody()->write(json_encode($nthPage));
+    return $response
+            ->withHeader('Content-Type', 'application/json');
+});
+$app->get('/allcompanies', function ($request, $response) use ($companies) {
+ //   $page = $request->getQueryParam('page', 1);
+ //   $per = $request->getQueryParam('per', 5);
+    $response->getBody()->write(json_encode($companies));
+    return $response
+            ->withHeader('Content-Type', 'application/json');
+});
+// END
+
 $app->run();
